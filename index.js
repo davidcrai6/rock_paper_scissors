a6e72c6ab1f9b0b26e664d39f0e7bf8901d9be1e
davidcrai6 <davidvcraig@gmail.com>
2023-04-15 10:55:06 -0500

    made a new function for the human choice in js, created dropdown button in html, created css file to style the button.

--- index.js
+++ index.js
@@ -4,4 +4,20 @@ function getComputerChoice() {
     return choice
 }
 
+function pitifulHumanChoice() {
+    document.getElementById("myDropdown").classList.toggle("show");
+    window.onclick = function(event) {
+        if (!EventTarget.target.matches('.dropbtn')) {
+            let dropdowns = document.getElementsByClassName("dropdown-content");
+            let i;
+            for (i = 0; i <dropdowns.length; i++) {
+                let openDropdown =dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classlist.remove('show');
+                }
+            }
+        }
+    }
+}
+
 
