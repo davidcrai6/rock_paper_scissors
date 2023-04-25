72efe0d26a7ae964c0dcef6a463f2ffd0d2b77e1
davidcrai6 <davidvcraig@gmail.com>
2023-04-25 18:15:02 -0500

    'Starting over'

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-function getComputerChoice() {
+    
+    function getComputerChoice() {
     let choices = ['rock', 'paper', 'scissors']
     let choice = choices[Math.floor(Math.random() * choices.length)];
     return choice
-}
-
-function pitifulHumanChoice() {
-    document.getElementById("myDropdown").classList.toggle("show");
-    window.onclick = function(event) {
-        if (!EventTarget.target.matches('.dropbtn')) {
-            let dropdowns = document.getElementsByClassName("dropdown-content");
-            let i;
-            for (i = 0; i <dropdowns.length; i++) {
-                let openDropdown =dropdowns[i];
-                if (openDropdown.classList.contains('show')) {
-                    openDropdown.classlist.remove('show');
-                }
-            }
-        }
     }
-}
 
 
